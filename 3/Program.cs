ffb719dd0251eb06e94240f8cc1509d8fe86aab2
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-15 13:46:12 +0500

    zadacha3

--- 3/Program.cs
+++ 3/Program.cs
@@ -2,12 +2,12 @@
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
-  case 1: Console.WriteLine("1->нет"); break;
-  case 2: Console.WriteLine("2->нет"); break;
-  case 3: Console.WriteLine("3->нет"); break;
-  case 4: Console.WriteLine("4->нет"); break;
-  case 5: Console.WriteLine("5->нет"); break;
-  case 6: Console.WriteLine("6->да"); break;
-  case 7: Console.WriteLine("6->да"); break;
+    case 1: Console.WriteLine("1->нет"); break;
+    case 2: Console.WriteLine("2->нет"); break;
+    case 3: Console.WriteLine("3->нет"); break;
+    case 4: Console.WriteLine("4->нет"); break;
+    case 5: Console.WriteLine("5->нет"); break;
+    case 6: Console.WriteLine("6->да"); break;
+    case 7: Console.WriteLine("6->да"); break;
 }
 Console.ReadKey();
